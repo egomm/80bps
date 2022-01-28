1f66d9ffb34ad316a23f801db165433301cf8174
egomm <egomyt@gmail.com>
2022-01-28 22:00:13 +1300

    Update LobbyChat.js

--- jsMacros/Macros/LobbyChat.js
+++ jsMacros/Macros/LobbyChat.js
@@ -1,6 +1,8 @@
 const sentence = event.text.toString()
-const word = 'unclaimed leveling reward';
-var words = `${sentence.includes(word) ? 'true' : 'false'}`;
-if(words == "true") {
-Chat.say("/play skyblock")
+const firstword = 'unclaimed leveling reward';
+var firstwords = `${sentence.includes(firstword) ? 'true' : 'false'}`;
+const secondword = 'You have reached your Hype limit'
+var secondwords = `${sentence.includes(secondword) ? 'true' : 'false'}`;
+if(firstwords == "true" || secondwords == "true") {
+    Chat.say("/play skyblock")
 }
